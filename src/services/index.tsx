82640df050a885b287fc2e7cb3f0163e7f8db50b
Yosmar Hinestroza <yosmar.hiinestroza@lsoftservicios.cl>
2024-03-12 00:41:11 -0300

    mode no cors

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -3,24 +3,29 @@ import { URL_API, headers } from './Config'
 export const webApiService = {
   getList: async () => {
     const urlApi = URL_API + 'dataUser'
-    const response = await fetch(urlApi, { method: 'GET', headers })
+    const response = await fetch(urlApi, {
+      mode: 'no-cors',
+      method: 'GET',
+      headers
+    })
     return await response.json()
   },
   getUserId: async (id: number | string) => {
     const urlApi = URL_API + 'dataUser/' + id
-    const response = await fetch(urlApi, { method: 'GET', headers })
+    const response = await fetch(urlApi, { mode: 'no-cors', method: 'GET', headers })
     return await response.json()
   },
 
   getUserBySearch: async (q: string) => {
     const urlApi = URL_API + 'dataUser?q=' + q
-    const response = await fetch(urlApi, { method: 'GET', headers })
+    const response = await fetch(urlApi, { mode: 'no-cors', method: 'GET', headers })
     return await response.json()
   },
   saveUserService: async (bodyUser: any) => {
     const urlApi = URL_API + 'dataUser'
     const response = await fetch(urlApi,
       {
+        mode: 'no-cors',
         method: 'post',
         body: JSON.stringify(bodyUser),
         headers
@@ -30,7 +35,7 @@ export const webApiService = {
   },
   deleteUserService: async (id: string | number) => {
     const urlApi = URL_API + `dataUser/${id}`
-    const response = await fetch(urlApi, { method: 'delete', headers })
+    const response = await fetch(urlApi, { mode: 'no-cors', method: 'delete', headers })
 
     return await response.ok
   }
